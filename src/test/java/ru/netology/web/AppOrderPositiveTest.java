9b954aa63b1f49742486e2425ecf551517709453
Sergey30ssw <133129423+Sergey30ssw@users.noreply.github.com>
2023-09-22 17:12:54 +0400

    Update AppOrderPositiveTest.java

--- src/test/java/ru/netology/web/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/web/AppOrderPositiveTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppOrderPositiveTest {
@@ -25,13 +27,13 @@ public class AppOrderPositiveTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        // options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
-    @AfterEach
-    public void afterEach() {
+     @AfterEach
+    void tearDown() {
         driver.quit();
         driver = null;
     }
@@ -42,8 +44,9 @@ public class AppOrderPositiveTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79061779689");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
-        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 }
